0c5928e6d1fd4b763460ee19aa0d527520ee0088
Jai Prakash <jp.pingal1993@gmail.com>
2022-05-17 16:35:27 +0530

    complated

--- src/components/BestSlipSelection.js
+++ src/components/BestSlipSelection.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Select from 'react-select'
 import "./index.css";
 const BestSlipSelection = () => {
 	const [data, setData] = useState([]);
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,10 @@ const  Header = () => {
 
     var date = new Date();
 date. setDate(date. getDate() - 13);
-// console. log( "in react 7 days",date);
-
     const addDays = (dateObj, numDays) =>{
         dateObj.setDate(dateObj.getDate() + numDays);
         return dateObj;
      }
-     
      let today = new Date().toLocaleDateString();
      let tomorrow = addDays(new Date(), 1);
      let firstDay =tomorrow.toLocaleDateString()
@@ -24,30 +21,11 @@ date. setDate(date. getDate() - 13);
      let fifthDay =fifth.toLocaleDateString()
      let sixth = addDays(new Date(), 4);
      let sixDay =sixth.toLocaleDateString()
-     
-     
-    //  console.log(
-    //      'Today: ' + today +
-    //      '\nTomorrow: ' + firstDay +
-    //      '\nThird-Day: ' + secondDay +
-    //      '\nForth-Day: ' + thirdDay +
-    //      '\nFifth-Day: ' + forthDay+
-    //      '\nSisth-Day: ' + fifthDay +
-    //      '\nSeventh-Day: ' + sixDay
-    //  );
-// let weekDates = []
-//     for (let i = 0; i < 7; i++) {
-//         let today = new Date();
-//         weekDates.push(today);
-//         today.setDate(new Date().getDate() + i);
-   
-//       }
-    
-//       console.log(weekDates)
 	return (
 		<div className="header-container">
 			<nav className='nav-contents'>
 				<span className='dates'>{today}</span>
+                <span className='dates'>{firstDay}</span>
 				<span className='dates'>{secondDay}</span>
 				<span className='dates'>{thirdDay}</span>
 				<span className='dates'>{forthDay}</span>
